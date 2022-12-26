3d6d5652f251d86ea39fc027f630ac40279e52f0
jyferreira <121454300+jyferreira@users.noreply.github.com>
2022-12-26 18:46:43 +0000

    Atualização main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,6 @@
-basic.forever(function () {
-	
+input.onButtonPressed(Button.A, function () {
+    basic.showNumber(randint(1, 23))
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showNumber(randint(1, 8))
 })
